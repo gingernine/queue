b7490d1d16cc2590a2a824331334aabf789854a5
gingernine <sy44895@gmail.com>
2016-11-21 13:33:51 +0900

    2016/11/22

---
+++ Mod_Bessel_1.R
@@ -0,0 +1,22 @@
+fact <- function(n){
+	if(n < 1){
+		return(1)
+	} else {
+		return(prod(1:n))
+	}
+}
+
+mod_bessel_1 <- function(x, j){
+	S <- 0
+	for (n in 0:100){
+		S <- S+((x/2)^(2*n + j))/fact(n)/fact(n+j)
+	}
+	return(S)
+}
+
+for(x in 1:20){
+	for(j in 1:20){
+		print( x*(mod_bessel_1(x,j-1) - mod_bessel_1(x, j+1)) )
+		print( 2*j*mod_bessel_1(x,j) )
+	}
+}
